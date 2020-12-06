e5ff407c4b22bb031fc8ab9f2025f1967dd424c3
Roy Schossberge <royschossberge@cs.huji.ac.il>
2020-12-06 19:51:54 +0200

    gaus

--- sol3.py
+++ sol3.py
@@ -7,13 +7,16 @@ from imageio import imread
 from skimage.color import rgb2gray
 import numpy as np
 import scipy as sp
-from scipy.special import comb
+import scipy.ndimage as spi
+
 
 # --------------------------- constants ----------------------------------------------
 
 MAX_SEGMENT = 255
 
 # -------------------------- functions -----------------------------------------------
+
+
 def build_gaussian_pyramid(im, max_levels, filter_size):
     """
     The function builds a gaussian pyramid.
@@ -25,12 +28,22 @@ def build_gaussian_pyramid(im, max_levels, filter_size):
     used for the pyramid construction.
     """
     pyr = [im]
-    filter_vec = np.empty((1, filter_size))
     conv_vec1 = np.ones((1, 2))
     conv_vec2 = np.ones((1, 2))
     for i in range(filter_size - 2):
         conv_vec1 = np.convolve(conv_vec1, conv_vec2)
-    filter_vec = (1 / sum(conv_vec1)) * conv_vec1
+    filter_vec = (1 / np.sum(conv_vec1)) * conv_vec1
+    n = im.shape[0]
+    new_image = np.copy(im)
+    i = 1
+    while n > 16 or i <= max_levels:
+        filtered_image = spi.filters.convolve(spi.filters.convolve(new_image, filter_vec, mode='constant', cval=0.0),
+                                              filter_vec.T, mode='constant', cval=0.0)
+        new_image = np.copy(filtered_image[::2, ::2])
+        pyr.append(new_image)
+        i += 1
+        n /= 4
+    return pyr, filter_vec
 
 
 def build_laplacian_pyramid(im, max_levels, filter_size):
@@ -43,7 +56,20 @@ def build_laplacian_pyramid(im, max_levels, filter_size):
     where each element of the array is a grayscale image and filter_vec which is row vector of shape (1, filter_size)
     used for the pyramid construction.
     """
+    gaus_pyr, gaus_filter = build_gaussian_pyramid(im, max_levels, filter_size)
+    lpyr = []
+    for i in range(1, len(gaus_pyr)):
+        lpyr.append(gaus_pyr[i - 1] - expand(gaus_pyr[i], gaus_filter))
+    return lpyr, gaus_filter * 2
 
+
+def expand(im, im_filter):
+    """
+    A helper method gets a matrix and expand it's size and blur it.
+    :param im: the matrix of the requested expand needed.
+    :param im_filter: the filter used for the expand action.
+    :return: expanded im.
+    """
 
 
 
